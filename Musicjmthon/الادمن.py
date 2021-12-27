dbbed7f5ad9c3b53962d13e5aba5ed880f3fa5b0
islam-200555 <96347863+islam-200555@users.noreply.github.com>
2021-12-27 19:04:40 +0200

    Add files via upload

--- Musicjmthon/الادمن.py
+++ Musicjmthon/الادمن.py
@@ -41,7 +41,7 @@ async def skip(client, m: Message):
             await m.reply(OP)
 
 
-@Client.on_message(filters.command(["انهاء", "ايقاف"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["انهاء", "end"], prefixes=f"{HNDLR}"))
 @authorized_users_only
 async def stop(client, m: Message):
     await m.delete()
